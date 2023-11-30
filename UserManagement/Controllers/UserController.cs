4df17599913855f030ae5cbd846690fa74a6f86c
Albert <abogevorgian@gmail.com>
2023-11-30 13:47:11 +0400

    add controllers

---
+++ UserManagement/Controllers/UserController.cs
@@ -0,0 +1,84 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserManagement.Core.Entities;
+using UserManagement.Core.Interfaces.Service;
+using UserManagement.Request;
+using UserManagement.Response;
+
+namespace UserManagement.Controllers;
+[Route("[controller]")]
+[ApiController]
+[Authorize]
+
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<ActionResult> Login(Login login)
+    {
+        var token = await _userService.Login(login.UserName, login.Password);
+
+        if (string.IsNullOrEmpty(token))
+        {
+            return Unauthorized();
+        }
+
+        return Ok(token);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetAllUsers()
+    {
+        var users = await _userService.GetAllUsersAsync();
+        return Ok(users.Adapt<IEnumerable<UserResponse>>());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUserById(int id)
+    {
+        var user = await _userService.GetUserByIdAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user.Adapt<UserResponse>());
+    }
+
+    [AllowAnonymous]
+    [HttpPost("CreateUser")]
+    public async Task<ActionResult<User>> CreateUser(UserRequest user)
+    {
+        var id = await _userService.CreateUser(user.Adapt<User>());
+
+        return Ok(id);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateUser(int id, User user)
+    {
+        if (id != user.Id)
+        {
+            return BadRequest();
+        }
+
+        _userService.UpdateUser(user);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        await _userService.DeleteUserAsync(id);
+        return NoContent();
+    }
+}
---
+++ UserManagement/Controllers/UsersProfileController.cs
@@ -0,0 +1,68 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserManagement.Core.Entities;
+using UserManagement.Core.Interfaces.Service;
+using UserManagement.Request;
+using UserManagement.Response;
+
+namespace UserManagement.Controllers;
+[Route("[controller]")]
+[ApiController]
+[Authorize]
+
+public class UsersProfileController : ControllerBase
+{
+    private readonly IUserProfileService _userProfileService;
+
+    public UsersProfileController(IUserProfileService userProfileService)
+    {
+        _userProfileService = userProfileService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UserProfile>>> GetAllUserProfiles()
+    {
+        var userProfiles = await _userProfileService.GetAllUserProfilesAsync();
+        return Ok(userProfiles.Adapt<UserProfileResponse>());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserProfile>> GetUserProfileById(int id)
+    {
+        var userProfile = await _userProfileService.GetUserProfileByIdAsync(id);
+
+        if (userProfile == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(userProfile.Adapt<UserProfileResponse>());
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UserProfile>> CreateUserProfile(UserProfileRequest userProfile)
+    {
+        var id = await _userProfileService.CreateUserProfile(userProfile.Adapt<UserProfile>());
+        return Ok(id);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateUserProfile(int id, UserProfile userProfile)
+    {
+        if (id != userProfile.Id)
+        {
+            return BadRequest();
+        }
+
+        _userProfileService.UpdateUserProfile(userProfile);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUserProfile(int id)
+    {
+        await _userProfileService.DeleteUserProfileAsync(id);
+        return NoContent();
+    }
+}
