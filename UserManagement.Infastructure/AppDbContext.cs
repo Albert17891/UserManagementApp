9042e71ed62ed7da32056e1ec14b29ff08a9d706
Albert <abogevorgian@gmail.com>
2023-11-30 11:50:28 +0400

    add db context

---
+++ UserManagement.Infastructure/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Core.Entities;
+
+namespace UserManagement.Infastructure;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> opt):base(opt)
+    {
+            
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserProfile> Profiles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
