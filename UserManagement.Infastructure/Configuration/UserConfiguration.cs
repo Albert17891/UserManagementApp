767c4a06236986f5257804a1ee809a7ce7035a33
Albert <abogevorgian@gmail.com>
2023-11-30 11:50:40 +0400

    add configuration

---
+++ UserManagement.Infastructure/Configuration/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserManagement.Core.Entities;
+
+namespace UserManagement.Infastructure.Configuration;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+
+        builder.HasOne(u => u.UserProfile)
+               .WithOne(up => up.User)
+               .HasForeignKey<UserProfile>(up => up.Id);
+    }
+}
