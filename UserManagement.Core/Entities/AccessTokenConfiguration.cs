c69da6c6d975d89bb0ca95918e1f7a57871e07b0
Albert <abogevorgian@gmail.com>
2023-11-30 13:47:02 +0400

    add new entity

---
+++ UserManagement.Core/Entities/AccessTokenConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManagement.Core.Entities;
+
+public class AccessTokenConfiguration
+{
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public string Key { get; set; }
+    public int AccessTokenExpiresMinutes { get; set; }
+}
--- UserManagement.Core/Entities/UserProfile.cs
+++ UserManagement.Core/Entities/UserProfile.cs
@@ -6,9 +6,7 @@ public class UserProfile
 {
     public int Id { get; set; } 
     public string FirstName {  get; set; }  
-    public string LastName { get; set; }
-    [Required]
-    [RegularExpression("^d{11}$",ErrorMessage ="Personal Number Must be 11")]
+    public string LastName { get; set; }   
     public string PersonalNumber {  get; set; }
 
     //Relations
