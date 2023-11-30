e020531dd3ca31392ca334a572e1796b3b241daf
Albert <abogevorgian@gmail.com>
2023-11-30 11:50:58 +0400

    add services extensions

---
+++ UserManagement.Infastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UserManagement.Infastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfastructureExtensions(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        return services;
+    }
+}
